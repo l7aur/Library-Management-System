38c6e145dcc53f38e1d2827e0ab04134c27ba418
l7aur <gradlaur@yahoo.com>
2025-03-05 10:01:10 +0200

    enabled postman testing

--- backend/src/main/java/com/laur/bookshop/security/SecurityConfig.java
+++ backend/src/main/java/com/laur/bookshop/security/SecurityConfig.java
@@ -1,9 +1,22 @@
-//package com.laur.demo.security;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//}
+package com.laur.bookshop.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)  // Disable CSRF (only for testing)
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll() // Allow all requests without authentication
+                );
+
+        return http.build();
+    }
+}
